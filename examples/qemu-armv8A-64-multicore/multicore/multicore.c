f756abdafce25e2e6706ca6519c5fc765764b2c7
Harshvardhan sharma <harshvardhanks@google.com>
2022-07-01 07:14:04 +0000

    Adds a command to multi-core framework
    Adds a "run" command to support running of function on all/specific platform core and passing of function arguments from prompt.
    
    Usage: To run function on all cores -
    	run all <function_name> <function_args>
    Usage: To run function on specific core -
    	run <core_id> <function_name> <function_args>
    
    Adds a test_function to check command execution.
    
    Change-Id: I21b6cf9e9719f9a9522bad0192bd8a263e669f26

--- examples/qemu-armv8A-64-multicore/multicore/multicore.c
+++ examples/qemu-armv8A-64-multicore/multicore/multicore.c
@@ -15,6 +15,11 @@
 */
 #include "multicore.h"
 
+#define MAX_ARG_COUNT 16
+
+static cmd func_addr;                   // holds targeted func address
+static int func_argc;                   // holds targeted func arg count
+static char *func_argv[MAX_ARG_COUNT];  // holds targeted func arguments
 static cmd cpu_function_addr[PLATFORM_CORE_COUNT];  // holds address of function
                                                     // triggered for each cpu
 
@@ -63,6 +68,36 @@ int test_fun2(int cpuid, char *argv[]) {
   for (int i = 0; i <= 10; i++) {
     multicore_printf("Core:%ld executing fun2\n", cpuid);
   }
+  return 0;
+}
+
+// test function - calculates +/-/* of two nums
+int test_func3(int argc, char *argv[]) {
+  if (argc != 4) {
+    multicore_printf("Wrong Usage\n");
+    multicore_printf("Usage: calc <num1> <num2> <+/-/*>\n");
+    return -1;
+  }
+  int num1 = atoi(argv[1]);
+  int num2 = atoi(argv[2]);
+  char expr = argv[3][0];
+  int result;
+  switch (expr) {
+    case '+':
+      result = num1 + num2;
+      break;
+    case '*':
+      result = num1 * num2;
+      break;
+    case '-':
+      result = num1 - num2;
+      break;
+    default:
+      multicore_printf("Invalid Expression: %s\n", argv[3]);
+      multicore_printf("Usage: calc <num1> <num2> <+/-/*>\n");
+      return -1;
+  }
+  multicore_printf("%ld\n", result);
   return 0;
 }
 
@@ -178,6 +213,95 @@ static int execute_on_each_cpu(int argc, char *argv[]) {
   return 0;
 }
 
+/*
+ * CPUs start execution from junction
+ * junction provides wrapper around the targeted func
+ * CPUs jumps to targeted func with func arg count and argv[]
+ */
+static void junction(int cpuid) {
+  multicore_printf("Core %ld starting execution\n", cpuid);
+  int result;
+  result = func_addr(func_argc, func_argv);
+  if (result == 0) {
+    multicore_printf("Core %ld: Succesful Execution\n", cpuid);
+  } else {
+    multicore_printf("Core %ld: Failed Execution\n", cpuid);
+  }
+  return;
+}
+
+// prints usage of run command
+void print_run_help(void) {
+  multicore_printf("Wrong Usage\n");
+  multicore_printf("Usage: To run function on all cores -  \n");
+  multicore_printf("\trun all <function_name> <function_args>\n");
+  multicore_printf("Usage: To run function on specific core -  \n");
+  multicore_printf("\trun <core_id> <function_name> <function_args>\n");
+}
+
+// triggered by run cmd, handles usage of run cmd
+static int execute(int argc, char *argv[]) {
+  if (argc < 3) {
+    print_run_help();
+    return -1;
+  }
+
+  // check cpu argument
+  int cpuid;
+  if (strcmp(argv[1], "all\0") == 0) {
+    cpuid = -1;  // all platform cpus
+  } else {       // specific cpu
+    cpuid = atoi(argv[1]);
+    if (cpuid < 0 || cpuid >= PLATFORM_CORE_COUNT) {
+      multicore_printf("Argument 2: Invalid Core Number: %s\n", argv[1]);
+      print_run_help();
+      return -1;
+    }
+  }
+
+  // check targeted func name & save address in func_addr
+  func_addr = get_function_addr(argv[2]);
+  if (func_addr == NULL) {
+    multicore_printf("Argument 3: Invalid Function Name: %s\n", argv[2]);
+    print_run_help();
+    return -1;
+  }
+
+  // save targeted function arg count
+  func_argc = argc - 2;
+  // save targeted function args
+  for (int arg = 0; arg < func_argc; arg++) {
+    func_argv[arg] = argv[arg + 2];
+  }
+
+  if (cpuid == -1) {  // run all platform cpus
+    multicore_printf("Starting Cores....\n\n");
+    for (int core_id = 1; core_id < PLATFORM_CORE_COUNT; core_id++) {
+      spin_cpu[core_id] =
+          (uint64_t)junction;  // save junction() addr in watch address
+    }
+  } else if (cpuid != 0) {  // run specific cpu
+    multicore_printf("Starting Core %ld....\n\n", cpuid);
+    spin_cpu[cpuid] =
+        (uint64_t)junction;  // save junction() addr in watch address
+  }
+
+  asm volatile("sev");
+
+  // run primary cpu
+  if (cpuid == -1 || cpuid == 0) {
+    junction(0);
+  }
+
+  // before returning to prompt for next function assigment,
+  // primary core waits till all secondary cores complete execution and are
+  // avilable
+  wait_for_cores();
+  return 0;
+}
+
+ADD_CMD(run, "Run function on specific/all core", execute);
+ADD_CMD(calc, "add/sub/mul two numbers", test_func3);
 ADD_CMD(Function1, "Test function", test_fun1);
 ADD_CMD(Function2, "Test function", test_fun2);
 ADD_CMD(run_task, "Deploy functions on given cores", function_execute);
