b43228d6151bffa06b613d05f848fd62c9b97784
Piyush Itankar <itankar@google.com>
2022-07-22 20:49:47 +0530

    Adds Raspberry-pi 2b example
    Signed-off-by: Piyush Itankar <itankar@google.com>
    Change-Id: I2da8b0dadef0926dc520460f132bbc9a013a4477

---
+++ examples/qemu-RPi2b/platform/uart/uart.h
@@ -0,0 +1,43 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+#ifndef __UART_H__
+#define __UART_H__
+
+#define PHY_PERI_BASE   (0x3F000000uL)
+#define UART_BASE (PHY_PERI_BASE + 0x00201000uL)
+
+/**
+ * @brief Put a character on uart data buffer
+ *
+ * @param c byte to be sent out
+ */
+void putc(char c);
+
+/**
+ * @brief Get a character from uart data buffer
+ *
+ * @return int byte read from the uart data register
+ */
+int getc(void);
+
+/**
+ * @brief Init the uart, set baud etc
+ *
+ */
+void uart_init(void);
+
+#endif
